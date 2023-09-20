18a626960e29f24af007415a66d5096520a901cd
ekosarr <ekoafi123@gmail.com>
2023-09-20 11:49:39 +0700

    modul 04

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// membuat route baru dengan method GET yang isinya kalimat halo dek
+const bodyPs = require("body-parser");
+app.use(bodyPs.urlencoded({ extended: false }));
+app.use(bodyPs.json());
+
 // import route posts
 const mhsRouter = require("./routes/mahasiswa");
 app.use("/api/mhs", mhsRouter);
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,22 +1,66 @@
 const express = require("express");
 const router = express.Router();
 
+const { body, validationResult } = require("express-validator");
+
 const connection = require("../config/db");
 
 router.get("/", function (req, res) {
-  connection.query("select * from mahasiswa order by id_m desc", function (err, rows) {
+  connection.query("select * from mahasiswa order by id_m desc", function (err, rows){
+});
+});
+
+router.post("/store", [
+  body("nama").notEmpty(), 
+  body("nrp").notEmpty(),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errors: errors.array(),
+    });
+  }
+  let Data = {
+    nama: req.body.nama,
+    nrp: req.body.nrp
+  };
+  connection.query("insert into mahasiswa set ?", Data, function (err, rows) {
     if (err) {
       return res.status(500).json({
         status: false,
-        message: "Server Failed",
+        message: "Server Error",
       });
     } else {
-      return res.status(200).json({
+      return res.status(201).json({
         status: true,
-        message: "Data Mahasiswa",
-        data: rows,
+        message: "Success!",
+        data: rows[0],
       });
     }
   });
 });
+
+router.get('/(:id)', function  (req, res){
+  let id = req.params.id;
+  connection.query(`select * from mahasiswa where id_m = ${id}`, function (err, rows) {
+    if(err){
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      })
+    }
+    if(rows.length <=0){
+      return res.status(408).json({
+        status: false,
+        message: "not found",
+      })
+    }else{
+      return res.status(200).json({
+        status: true,
+        message: "data mahasiswa",
+        data: rows[0]
+      })
+    }
+  })
+})
 module.exports = router;
